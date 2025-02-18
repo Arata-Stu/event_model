565a5c924052e8e9f35c693560e5cd6781a8858b
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-02-19 00:42:47 +0900

    update

--- models/neck/build_neck.py
+++ models/neck/build_neck.py
@@ -7,10 +7,12 @@ from .yolo_pafpn import YOLOPAFPN
 
 def build_neck(neck_cfg: DictConfig, in_channels: Tuple[int, ...]) -> torch.nn.Module:
     if neck_cfg.name == 'PAFPN':
-        neck = YOLOPAFPN(in_channels=in_channels,
+        neck = YOLOPAFPN(depth=neck_cfg.depth,
+                         in_stages=neck_cfg.in_stages,
+                         in_channels=in_channels,
                          depthwise=neck_cfg.depthwise,
                          act=neck_cfg.act,
-                         inplanes=neck_cfg.inplanes)
+                         compile_cfg=neck_cfg.compile)
     else:
         raise NotImplementedError(f"Neck {neck_cfg.name} is not implemented")
     return neck
