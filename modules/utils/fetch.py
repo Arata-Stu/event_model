3047c8d857e0324db7f850dad83c8dea96d83377
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-02-18 13:45:39 +0900

    add code

--- modules/utils/fetch.py
+++ modules/utils/fetch.py
@@ -1,8 +1,13 @@
 import pytorch_lightning as pl
 from omegaconf import DictConfig
-
 from modules.data.data_module import DataModule 
+from modules.model.rnn_module import  ModelModule as rnn_det_module
 
+def fetch_model_module(config: DictConfig) -> pl.LightningModule:
+    model_str = config.model.name
+    if model_str == 'rnndet':
+        return rnn_det_module(config)
+    raise NotImplementedError
 
 def fetch_data_module(config: DictConfig) -> pl.LightningDataModule:
     batch_size_train = config.batch_size.train
