cef5324afcdd2bd2705c383c1e741ab79626df67
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-02-19 00:20:22 +0900

    updatea neck

--- models/neck/build_neck.py
+++ models/neck/build_neck.py
@@ -6,7 +6,7 @@ from omegaconf import DictConfig
 from .yolo_pafpn import YOLOPAFPN
 
 def build_neck(neck_cfg: DictConfig, in_channels: Tuple[int, ...]) -> torch.nn.Module:
-    if neck_cfg.name == 'YOLOPAFPN':
+    if neck_cfg.name == 'PAFPN':
         neck = YOLOPAFPN(in_channels=in_channels,
                          depthwise=neck_cfg.depthwise,
                          act=neck_cfg.act,
