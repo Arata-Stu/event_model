fe19383e684a3b8d5cdae5cba75dd505a50021eb
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-02-18 23:45:39 +0900

    update

--- models/head/yolox/models/yolo_head.py
+++ models/head/yolox/models/yolo_head.py
@@ -12,7 +12,7 @@ try:
 except ImportError:
     th_compile = None
 
-from models.detection.yolox.utils import bboxes_iou
+from models.head.yolox.utils import bboxes_iou
 
 from .losses import IOUloss
 from .network_blocks import BaseConv, DWConv
