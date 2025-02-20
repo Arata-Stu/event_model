f27dffb8ce451ac6b024039be3d82ddd5431d2cf
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-02-21 08:20:35 +0900

    bit change

--- models/layers/yolox/models/yolo_head.py
+++ models/layers/yolox/models/yolo_head.py
@@ -501,7 +501,7 @@ class YOLOXHead(nn.Module):
         if mode == "cpu":
             cls_preds_, obj_preds_ = cls_preds_.cpu(), obj_preds_.cpu()
 
-        with torch.cuda.amp.autocast(enabled=False):
+        with torch.amp.autocast(dtype=torch.float32):
             cls_preds_ = (
                 cls_preds_.float().sigmoid_() * obj_preds_.float().sigmoid_()
             ).sqrt()
