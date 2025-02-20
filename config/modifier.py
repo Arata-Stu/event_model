80849afae3db8da93c763781ffa84d850a0369b8
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-02-20 18:08:41 +0900

    update

--- config/modifier.py
+++ config/modifier.py
@@ -24,7 +24,7 @@ def dynamically_modify_train_config(config: DictConfig):
         if mdl_name in {'rvt'}:
             backbone_cfg = mdl_cfg.backbone
             backbone_name = backbone_cfg.name
-            if backbone_name == 'MaxViTRNN':
+            if backbone_name == 'RVT':
                 partition_split_32 = backbone_cfg.partition_split_32
                 assert partition_split_32 in (1, 2, 4)
 
